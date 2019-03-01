a62f2c38ee254ac933a199c0a59a75847539c916
Krzysztof_Tomkow <krzysztof.tomkow@gmail.com>
2019-03-01 12:23:01 +0100

    Changed most important method name

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 
 @app.route('/<string:username>/<decision>')
-def show_user_profile(username, decision):
+def register_user_decision(username, decision):
     return 'User{0} decision: {1}'.format(username, decision)
 
 
