5730b9d98f436b672b843bd559be28692ed78859
Krzysztof_Tomkow <krzysztof.tomkow@gmail.com>
2019-03-04 20:13:02 +0100

    Rewrite service with decisions

--- addresses_service.py
+++ addresses_service.py
@@ -1,16 +1,32 @@
 import jsonpickle
 
 
-def save_decisions(addresses_and_decisions, filename="decisions.json"):
+def get_dictionary(filename="decisions.json"):
+    try:
+        file = open(filename, "r")
+        frozen = file.read()
+        addresses_and_decisions = jsonpickle.decode(frozen)
+        file.close()
+        return addresses_and_decisions
+    except:
+        reset(filename)
+        return get_dictionary(filename)
+
+
+def save_dictionary(dictionary, filename="decisions.json"):
+    frozen = jsonpickle.encode(dictionary)
     file = open(filename, "w")
-    frozen = jsonpickle.encode(addresses_and_decisions)
     file.write(frozen)
     file.close()
 
 
-def get_decisions(filename="decisions.json"):
-    file = open(filename, "r")
-    frozen = file.read()
-    addresses_and_decisions = jsonpickle.decode(frozen)
-    file.close()
-    return addresses_and_decisions
+def reset(filename="decisions.json"):
+    save_dictionary([])
+
+
+def add_decision(email:str, decision:int, filename="decisions.json"):
+    dictionary = get_dictionary()
+    email = email.strip().lower()
+    if not email in dictionary:
+        dictionary[email] = decision
+        save_dictionary(dictionary)
