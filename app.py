73c7c01a821ff84910f295efef213cd430a3f324
Krzysztof_Tomkow <krzysztof.tomkow@gmail.com>
2019-03-05 11:34:46 +0100

    Testing scheduler

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
 import config
 import decisions_service
+
+
+import time
+import atexit
+import datetime
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
 app = Flask(__name__)
 
 
@@ -20,5 +29,29 @@ def print_config():
     return conf.print_as_html()
 
 
+@app.route("/dict")
+def print_dictionary_with_decisions():
+    dictionary = decisions_service.get_dictionary()
+    return dictionary.__str__()
+
+
+@app.route("/datetime")
+def print_server_datetime():
+    return datetime.datetime.now().__str__()
+    
+
+def do_something():
+    print("Doing something")
+    decisions_service.reset()
+    decisions_service.add_decision("krzysztof.tomkow@gmail.com", 1000)
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=do_something, trigger="cron", hour = "11", minute = "31")
+scheduler.start()
+
+atexit.register(lambda: scheduler.shutdown)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(use_reloader=False)
