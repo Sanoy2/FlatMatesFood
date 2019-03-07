02bf7bb38c5745d21f2f43dae53ef98dc70619a3
Krzysztof_Tomkow <krzysztof.tomkow@gmail.com>
2019-03-07 20:24:48 +0100

    Removed temporary methods

--- app.py
+++ app.py
@@ -28,31 +28,13 @@ mail_settings = {
 app.config.update(mail_settings)
 
 
-@app.route("/<string:email>/<decision>")
+@app.route("/<string:email>/<decision:int>")
 def register_user_decision(email, decision):
     decision_saved = decisions_service.add_decision(email, decision)
     if decision_saved:
         return "User {0} decision: {1} registered".format(email, decision)
     else:
         return "User {0} decision has already been registered today".format(email)
-
-
-@app.route("/config")
-def print_config():
-    conf = config.config_data.load_config()
-    print(conf)
-    return conf.print_as_html()
-
-
-@app.route("/dict")
-def print_dictionary_with_decisions():
-    dictionary = decisions_service.get_dictionary()
-    return dictionary.__str__()
-
-
-@app.route("/datetime")
-def print_server_datetime():
-    return datetime.datetime.now().__str__()
     
 
 def question_action():
