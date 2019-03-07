3431706e74004b0af14b5448e30c1773d1b0cefc
Krzysztof_Tomkow <krzysztof.tomkow@gmail.com>
2019-03-07 18:34:33 +0100

    Removed procfile

--- app.py
+++ app.py
@@ -36,33 +36,6 @@ def register_user_decision(email, decision):
     else:
         return "User {0} decision has already been registered today".format(email)
 
-<<<<<<< HEAD
-@app.route('/')
-def hello():
-    return 'hello'
-
-
-@app.route('/config')
-=======
-
-@app.route("/config")
->>>>>>> dev
-def print_config():
-    conf = config.config_data.load_config()
-    print(conf)
-    return conf.print_as_html()
-
-
-@app.route("/dict")
-def print_dictionary_with_decisions():
-    dictionary = decisions_service.get_dictionary()
-    return dictionary.__str__()
-
-
-@app.route("/datetime")
-def print_server_datetime():
-    return datetime.datetime.now().__str__()
-
 
 def question_action():
     decisions_service.reset()
